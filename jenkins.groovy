cd9ecf7afcc0d095f6bc1737574be83f8bd0d779
Pooja Paturkar <pujapaturkar99@gmail.com>
2025-03-28 16:23:13 +0530

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,7 +1,7 @@
 pipeline {
     agent any
     stages {
-        stage('git-hhjv') {
+        stage('git-hhjvjk') {
             steps {
                 git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
             }
