eb94a0ddd1508a6fae786b5db9331956201f1e4b
Pooja Paturkar <pujapaturkar99@gmail.com>
2025-03-28 16:20:24 +0530

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,7 +1,7 @@
 pipeline {
     agent any
     stages {
-        stage('git_checkout') {
+        stage('git-clone') {
             steps {
                 git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
             }
