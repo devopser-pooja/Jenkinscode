1a41e974745fd114fbbec76646b1d390afb1e181
Pooja Paturkar <pujapaturkar99@gmail.com>
2025-03-28 16:15:08 +0530

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,1 +1,26 @@
-
+pipeline {
+    agent any
+    stages {
+        stage('git_checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
+            }
+        }
+        stage('build') {
+            steps {
+                sh 'echo "Build successfully done"'
+            }
+        }
+        stage('test') {
+            steps {
+                sh 'echo "Test successfully done"'
+            }
+        }
+        stage('deploy') {
+            steps {
+                sh 'echo "Deploy step placeholder"'
+                
+            }
+        }
+    }
+}
