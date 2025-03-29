8eb626a85402096e991c66984de008cc5989571c
Pooja Paturkar <pujapaturkar99@gmail.com>
2025-03-29 20:06:58 +0530

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -14,22 +14,15 @@ pipeline {
         }
         stage('test') {
             steps {
-                withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-cred') {
-                    sh '/opt/maven/bin/mvn sonar:sonar'
+                
                     sh 'echo "Test successfully done"'
                 }
             }
         }
-        stage('quality-gate') { // Fixed indentation & added missing braces
-            steps {
-                timeout(time: 5, unit: 'MINUTES') { // Added unit for clarity
-                    waitForQualityGate abortPipeline: true
-                }
-            }
-        }
+
         stage('deploy') { // Moved inside stages block
             steps {
-               deploy adapters: [tomcat9(credentialsId: 'Tomcat', path: '', url: 'http://172.31.34.191:8080')], contextPath: '/', war: '**/*.war'
+            
                 sh 'echo "Deploy step placeholder"'
             }
         }
