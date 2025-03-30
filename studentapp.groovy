7aef1dd9f78d0c5530aec4f588f506fc64358c70
Pooja Paturkar <pujapaturkar99@gmail.com>
2025-03-30 21:11:44 +0530

    Create studentapp.groovy

---
+++ studentapp.groovy
@@ -0,0 +1,45 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Git clone') {
+            steps {
+                sh 'git clone https://github.com/Anilbamnote/student-ui-app.git'
+                sh 'echo "Git repo cloned successfully"'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                sh '/opt/maven/bin/mvn clean package'
+                sh 'echo "Build successful"'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-cred') {
+                    sh '/opt/maven/bin/mvn sonar:sonar'
+                }
+                sh 'echo "Test successful"'
+            }
+        }
+
+        stage('Quality Gates') {
+            steps {
+                timeout(time: 5, unit: 'MINUTES') {
+                    waitForQualityGate abortPipeline: true
+                }
+            }
+        }
+
+        stage('Deploy to Tomcat') {
+            steps {
+                deploy adapters: [tomcat9(credentialsId: 'user-of-tomcat', path: '', url: 'http://172.31.36.201:8080')],
+                       contextPath: '/',
+                       war: '**/*.war'
+                sh 'echo "Deployment successful"'
+            }
+        }
+    }
+}
